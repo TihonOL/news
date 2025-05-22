0fdb641d7217dea5e464ae97e96613a0c091621c
IvaTAT <ivan208@mail.ru>
2025-05-22 17:45:24 +0300

    relise_1

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/tokens', tokenRouter);
 // app.use('api/categries', categoriesRouter);
-// app.use('/api/news', newsRouter);
+app.use('/api/news', newsRouter);
 // app.use('/api/profile', profileRouter);
 
 // app.use('/api/news', newsRouter);
