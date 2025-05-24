380a6d86fae736a94a4cf13fd2ecd40471471ec0
Tihon <Ltihonol@yandex.ru>
2025-05-24 13:06:27 +0300

    pre deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'build', // например, изменить папку вывода на 'build'
+    outDir: '../server/dist',
   },
+  base: './',
 });
--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const path = require('path');
 const authRouter = require('./routes/auth.router');
 const tokenRouter = require('./routes/token.router');
 
@@ -18,15 +19,14 @@ app.use(express.json());
 
 app.use('/api/auth', authRouter);
 app.use('/api/tokens', tokenRouter);
-app.use('/api/categories', categoriesRouter)
+app.use('/api/categories', categoriesRouter);
 app.use('/api/news', newsRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/whitelist', whitelistRouter);
 
-
-// app.use('/api/news', newsRouter);
-// app.use('/api/profile', profileRouter);
-
-
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
 
 module.exports = app;
