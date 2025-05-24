cba97a41835f2f82bb4185a3672cd863d0e0d941
Tihon <Ltihonol@yandex.ru>
2025-05-24 13:42:51 +0300

    app.get changed on regexp

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
+
 const authRouter = require('./routes/auth.router');
 const tokenRouter = require('./routes/token.router');
-
 const newsRouter = require('./routes/newsRouter');
 const profileRouter = require('./routes/profileRouter');
 const categoriesRouter = require('./routes/categories.router');
@@ -18,14 +18,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+
 app.use('/api/tokens', tokenRouter);
+
 app.use('/api/categories', categoriesRouter);
+
 app.use('/api/news', newsRouter);
+
 app.use('/api/profile', profileRouter);
+
 app.use('/api/whitelist', whitelistRouter);
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
-app.get('/*', (req, res) => {
+
+app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
